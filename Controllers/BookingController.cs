a3e7705a9493d8fc4486161910a6e09f1ed81e76
Sajesh Kumar <sajesh_kumar@cms.co.in>
2022-09-22 16:18:21 +0530

    first commit

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceBookingLibrary.Models;
+using BookingManagement.Repo;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ProductManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly TrainingServiceBookingContext _context;
+
+        public BookingController(TrainingServiceBookingContext db)
+        {
+            _context = db;
+        }
+
+        [Authorize()]
+        [HttpPost]
+        public async Task<ActionResult<Appservicerequest>> Add(Appservicerequest u)
+        {
+            BookingRepo bookingRepo = new BookingRepo(_context);
+
+            return await bookingRepo.AddNewRequest(u);
+        }
+
+        [Authorize()]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Appservicerequest>> UpdateRequest(int id, Appservicerequest p)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.UpdateRequest(id, p);
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+
+        [Authorize()]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Appservicerequest>>> GetAll()
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+                return await bookingRepo.getAllRequests();
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteRequest(int id)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+                bookingRepo.DeleteRequest(id);
+                return Ok();
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Appservicerequest>> Get(int id)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getRequest(id);
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ByUserId/{uid}")]
+        public async Task<ActionResult<List<Appservicerequest>>> GetByUserId(int uid)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getRequestByUserId(uid);
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ByStatus/{status}")]
+        public async Task<ActionResult<List<Appservicerequest>>> GetByStatus(string status)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getRequestByStatus(status);
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ByUserByStatus/{id}/{status}")]
+        public async Task<ActionResult<List<Appservicerequest>>> GetByUidAndStatus(int id,string status)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getRequestByUserIdAndStatus(id,status);
+            }
+            catch (NoRequestsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpPost("report")]
+        public async Task<ActionResult<Appservicerequestreport>> AddReport(Appservicerequestreport u)
+        {
+            BookingRepo bookingRepo = new BookingRepo(_context);
+
+            return await bookingRepo.AddNewReport(u);
+        }
+
+        [Authorize()]
+        [HttpGet("report")]
+        public async Task<ActionResult<IEnumerable<Appservicerequestreport>>> GetAllReports()
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+                return await bookingRepo.getAllReports();
+            }
+            catch (NoReportsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ReportById/{id}")]
+        public async Task<ActionResult<Appservicerequestreport>> GetReportByReportId(int id)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getReport(id);
+            }
+            catch (NoReportsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ReportByUserId/{uid}")]
+        public async Task<ActionResult<List<Appservicerequestreport>>> GetReportByUserId(int uid)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getReportByUserId(uid);
+            }
+            catch (NoReportsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [Authorize()]
+        [HttpGet("ReportByRequestId/{rid}")]
+        public async Task<ActionResult<List<Appservicerequestreport>>> GetReportByRequestId(int rid)
+        {
+            try
+            {
+                BookingRepo bookingRepo = new BookingRepo(_context);
+
+                return await bookingRepo.getReportByRequestId(rid);
+            }
+            catch (NoReportsException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+
+    }
+}
